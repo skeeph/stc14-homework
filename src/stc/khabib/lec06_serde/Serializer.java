1e0c4a96cd77c37e8ad82d7e7435f2d46acd3cbe
Khabib Murtuzaaliev <skeeph05@gmail.com>
2019-01-22 09:48:37 +0300

    Рефракторинг сериализации

--- src/stc/khabib/lec06_serde/Serializer.java
+++ src/stc/khabib/lec06_serde/Serializer.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Serializer {
+    private static final String IDENT = "   ";
     private final Set<String> primitiveWrappers = new HashSet<>(Arrays.asList(
             "Byte", "Short", "Integer", "Long", "Float", "Double", "Character", "Boolean", "String"
     ));
@@ -15,60 +16,54 @@ public class Serializer {
         String name = o.getClass().getCanonicalName();
         StringBuilder sb = new StringBuilder();
         sb.append("{\"className\": \"").append(name).append("\",");
-        String objectJson = serializeObject(o);
+        String objectJson = serializeObject(o, 0);
         sb.append("\"fields\":").append(objectJson).append("}");
         System.out.println(sb.toString());
     }
 
-    private String serializeObject(Object o) throws IllegalAccessException {
+    private String serializePrimitive(Object o) {
+        String res = o.toString();
+        if (o instanceof String) {
+            res = "\"" + res + "\"";
+        }
+        return res;
+    }
+
+    private String serializeObject(Object o, int identLevel) throws IllegalAccessException {
         if (o == null) {
             return "null";
         }
         Class type = o.getClass();
         if (type.isPrimitive() || primitiveWrappers.contains(type.getSimpleName())) {
-            String res = o.toString();
-            if (type.getSimpleName().equals("String")) {
-                res = "\"" + res + "\"";
-            }
-            return res;
+            return this.serializePrimitive(o);
         }
         StringBuilder sb = new StringBuilder();
         sb.append("{");
-        String field;
 
         Field[] declaredFields = o.getClass().getDeclaredFields();
         for (int i = 0; i < declaredFields.length; i++) {
-            Field declaredField = declaredFields[i];
-            declaredField.setAccessible(true);
-            field = serializeField(o, declaredField);
-            sb.append(field);
+            Field f = declaredFields[i];
+            f.setAccessible(true);
+
+            StringBuilder fieldSb = new StringBuilder();
+            Class fieldType = f.getType();
+            fieldSb.append("\"").append(f.getName()).append("\":");
+
+            if (fieldType.isArray()) {
+                String arrayValue = serializeArray(o, f);
+                fieldSb.append(arrayValue);
+            } else {
+                String objectValue = serializeObject(f.get(o), i + 1);
+                fieldSb.append(objectValue);
+            }
+
+            sb.append(fieldSb.toString());
             if (i < declaredFields.length - 1) {
-                sb.append(",");
+                sb.append(",\n");
             }
         }
         sb.append("}");
         return sb.toString();
-    }
-
-    private String serializeField(Object o, Field f) throws IllegalAccessException {
-        StringBuilder fieldSb = new StringBuilder();
-        Class fieldType = f.getType();
-        fieldSb.append("\"").append(f.getName()).append("\":");
-
-        if (fieldType.isPrimitive() || primitiveWrappers.contains(fieldType.getSimpleName())) {
-            if (fieldType.getSimpleName().equals("String")) {
-                fieldSb.append("\"").append(f.get(o)).append("\"");
-            } else {
-                fieldSb.append(f.get(o));
-            }
-        } else if (fieldType.isArray()) {
-            String arrayValue = serializeArray(o, f);
-            fieldSb.append(arrayValue);
-        } else {
-            String objectValue = serializeObject(f.get(o));
-            fieldSb.append(objectValue);
-        }
-        return fieldSb.toString();
     }
 
     private String serializeArray(Object o, Field array) throws IllegalAccessException {
@@ -78,7 +73,7 @@ public class Serializer {
         int length = Array.getLength(arr);
         for (int i = 0; i < length; i++) {
             Object arrayElement = Array.get(arr, i);
-            sb.append(serializeObject(arrayElement));
+            sb.append(serializeObject(arrayElement, 1));
             if (i < length - 1) {
                 sb.append(", ");
             }
